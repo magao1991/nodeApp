974ad8d3e5fffeebb89ef5cb7645f1e02b5040df
magao1991 <756278475@qq.com>
2015-05-28 01:24:39 +0800

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,40 @@
-var gulpLoadPlugins = require('gulp-load-plugins'),
+var gulp = require('gulp'),
+	gulpLoadPlugins = require('gulp-load-plugins'),
 	plugins = gulpLoadPlugins(),
 	port = 35792;
 
 var paths = {
 	css: {
-		src: './src/less/**/*.less',
-		dest: './dist/css',
-		inc: './src/less/includes',
+		src: './src/css/**/*.less',
+		inc: './src/css/includes',
+		dist: './dist/css',
 		filter: []
 	},
 	scripts: {
-		src: './src/less',
-		dest: './dist/css'
+		src: './src/js/**/*.js',
+		dist: './dist/js',
 		filter: []
 	},
 	images: {
-		src: './src/less',
-		dest: './dist/css'
+		src: './src/images/**/*',
+		dist: './dist/images',
 		filter: []
 	}
 }
 
 // 样式处理
 gulp.task('less', function() {
-	gulp.src(path.css.src).pipe(plugins.less({
-		paths: [paths.css.inc],
-		plugins: []
-	}))
+	return gulp.src(paths.css.src).pipe(plugins.less({
+		paths: [paths.css.inc]
+	})).pipe(gulp.dest(paths.css.dist));
+});
+
+//清理文件
+gulp.task('clean', function() {
+	return gulp.src([paths.css.dist], {read: false}).pipe(plugins.clean());
+});
+
+//默认任务
+gulp.task('default', ['clean', 'less'], function() {
+	console.log('suc');
 })
